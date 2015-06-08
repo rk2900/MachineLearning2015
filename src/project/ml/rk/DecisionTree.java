d49a085e947f5cebf81ec0fe4211b92b081b1c4f
Ren Kan <rk6556@gmail.com>
2015-06-08 14:58:58 +0800

    [dev] Topic Model and Decision Tree are added

---
+++ src/project/ml/rk/DecisionTree.java
@@ -0,0 +1,29 @@
+package project.ml.rk;
+
+import java.util.List;
+
+import project.ml.hwy.Data;
+import project.ml.hwy.Model;
+import project.ml.hwy.Result;
+
+public class DecisionTree extends Model {
+
+	@Override
+	protected void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected List<Result> predict(List<Data> dataList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void train(List<Data> dataList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
