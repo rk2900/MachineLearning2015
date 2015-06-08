463848f60afec1762366bcacecdf9f89eff70160
Zhu Chenhao <zhuchenhao1992@gmail.com>
2015-06-08 15:16:47 +0800

    basic

---
+++ src/project/ml/chzhu/MutualInfromation.java
@@ -0,0 +1,29 @@
+package project.ml.chzhu;
+
+import java.util.List;
+
+import project.ml.hwy.Data;
+import project.ml.hwy.Model;
+import project.ml.hwy.Result;
+
+public class MutualInfromation extends Model{
+
+	@Override
+	protected void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected List<Result> predict(List<Data> dataList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void train(List<Data> dataList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
