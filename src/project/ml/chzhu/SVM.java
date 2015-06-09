707051530929737f7968974f2e062a02db6d6efb
Zhu Chenhao <zhuchenhao1992@gmail.com>
2015-06-09 15:36:41 +0800

    update the SVM file

--- src/project/ml/chzhu/SVM.java
+++ src/project/ml/chzhu/SVM.java
@@ -1,5 +1,6 @@
 package project.ml.chzhu;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import project.ml.hwy.Data;
@@ -17,13 +18,17 @@ public class SVM extends Model {
 	@Override
 	protected List<Result> predict(List<Data> dataList) {
 		// TODO Auto-generated method stub
-		return null;
+		LinkedList<Result> results = new LinkedList<Result>();
+		return results;
 	}
 
 	@Override
 	protected void train(List<Data> dataList) {
 		// TODO Auto-generated method stub
-		
+		System.out.println(dataList.size());
+//		System.out.println(dataList.get(1).getPic());
+//		System.out.println(dataList.get(1).getPoi());
+		System.out.println(dataList.get(0).getContent());
 	}
 
 }
